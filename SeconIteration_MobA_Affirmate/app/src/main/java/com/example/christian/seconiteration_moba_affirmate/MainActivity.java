b2f93936a4db1b03959f7ce56c7b17a7c8c94212
Chris <sutter.christian95@gmail.com>
2018-12-08 21:38:52 +0100

    Added font and button label change

--- SeconIteration_MobA_Affirmate/app/src/main/java/com/example/christian/seconiteration_moba_affirmate/MainActivity.java
+++ SeconIteration_MobA_Affirmate/app/src/main/java/com/example/christian/seconiteration_moba_affirmate/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.christian.seconiteration_moba_affirmate;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView myTextView;
+    Typeface montserratFont;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +25,46 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        myTextView = findViewById(R.id.TextView);
+        montserratFont = Typeface.createFromAsset(this.getAssets(), "fonts/Montserrat-ExtraBold.ttf");
+        myTextView.setTypeface(montserratFont);
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        Button editButton = (Button) findViewById(R.id.editButtonPressed);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Edit", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button disableButton = (Button) findViewById(R.id.disableButtonPressed);
+        disableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            // int count = 0;
+            public void onClick(View view) {
+                if(count%2 == 1 ){
+                    disableButton.setText("Disable");
+                    Toast.makeText(MainActivity.this, "Enabled", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    disableButton.setText("Enable");
+                    Toast.makeText(MainActivity.this, "Disabled", Toast.LENGTH_SHORT).show();
+                }
+
+                count++;
+                if(count == 10){count = 0;}
+
+
             }
         });
     }
