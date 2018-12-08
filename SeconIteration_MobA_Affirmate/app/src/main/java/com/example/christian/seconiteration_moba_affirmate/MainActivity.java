93a73750d22dff630f800a12df73dea51dcc2828
Chris <sutter.christian95@gmail.com>
2018-12-08 22:31:31 +0100

    tried stuff, broke it, made it work again

--- SeconIteration_MobA_Affirmate/app/src/main/java/com/example/christian/seconiteration_moba_affirmate/MainActivity.java
+++ SeconIteration_MobA_Affirmate/app/src/main/java/com/example/christian/seconiteration_moba_affirmate/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
